80fbaf31569a78e441ab0dcfe4851316552be14b
candrist <elderandrist@hotmail.com>
2016-06-06 10:24:43 -0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 pkill -f /home/pi/Applications/MMDVMHost
-cat /dev/null > /home/pi/.bash_history
 rm -Rf /home/pi/MMDVM_*.ini
 rm -Rf /home/pi/Logs/*
 rm -Rf /home/pi/Applications/MMDVMHost/MMDVM-*.log
 rm -Rf /usr/local/var/log/opendv/*
+cat /dev/null > /home/pi/.bash_history
+history -c
