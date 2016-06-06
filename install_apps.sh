3735cf10792c5c9fc70673314d8535c82f6b43cc
candrist <elderandrist@hotmail.com>
2016-06-06 08:37:36 -0600

    Update and rename install_arduino.sh to install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -11,3 +11,13 @@ sudo mv /home/pi/.local/share/applications/arduino-arduinoide.desktop /usr/share
 rm -Rf /home/pi/Desktop/arduino-arduinoide.desktop
 rm -Rf /home/pi/.gnome/apps/arduino-arduinoide.desktop
 rm -Rf /home/pi/Downloads/arduino-1.6.9-linuxarm.tar.xz
+
+git clone https://github.com/g4klx/MMDVM.git /home/pi/Applications/MMDVM
+git clone https://github.com/g4klx/MMDVMHost.git /home/pi/Applications/MMDVMHost
+git clone https://github.com/g4klx/MMDVMCal.git /home/pi/Applications/MMDVMCal
+git clone https://github.com/dl5di/OpenDV.git /home/pi/Applications/OpenDV
+
+#cd /home/pi/Applications/OpenDV/ircDDBGateway
+#./configure
+#make
+#sudo make install
