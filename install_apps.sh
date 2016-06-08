432de032dd67b94f72cb59bdc4ade045b4cc1422
candrist <elderandrist@hotmail.com>
2016-06-08 06:43:24 -0600

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -19,8 +19,15 @@ rm -Rf /home/pi/Downloads/arduino-1.6.9-linuxarm.tar.xz
 git clone https://github.com/g4klx/MMDVM.git /home/pi/Applications/MMDVM
 git clone https://github.com/g4klx/MMDVMHost.git /home/pi/Applications/MMDVMHost
 git clone https://github.com/g4klx/MMDVMCal.git /home/pi/Applications/MMDVMCal
-git clone https://github.com/dl5di/OpenDV.git /home/pi/Applications/OpenDV
-#Build ircDDBGateway
+#Install ircDDBGateway From DEB
+sudo apt-get -y install libwxbase2.8-0 libwxgtk2.8-0 gawk ntpdate libsigsegv2
+wget -O /home/pi/Downloads/opendvdata_20151116-2_all.deb http://download.prgm.org/dl5di-soft/opendv/repositories/raspbian/opendvdata_20151116-2_all.deb
+wget -O /home/pi/Downloads/ircddbgateway_20151116-2_armhf.deb http://download.prgm.org/dl5di-soft/opendv/repositories/raspbian/ircddbgateway_20151116-2_armhf.deb
+sudo dpkg -i /home/pi/Downloads/opendvdata_20151116-2_all.deb
+sudo dpkg -i /home/pi/Downloads/ircddbgateway_20151116-2_armhf.deb
+rm -Rf /home/pi/Downloads/opendvdata_20151116-2_all.deb /home/pi/Downloads/ircddbgateway_20151116-2_armhf.deb
+#Build ircDDBGateway From Source
+# git clone https://github.com/dl5di/OpenDV.git /home/pi/Applications/OpenDV
 # cd /home/pi/Applications/OpenDV/ircDDBGateway
 # ./configure
 # make
