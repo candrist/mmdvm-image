c4321756b17bb262984ea3404b3f313c7c9ed31d
candrist <elderandrist@hotmail.com>
2016-06-07 18:42:56 -0600

    Update install.sh
    added comments

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Update, Upgrade and Install Required Software
 sudo apt-get update /
 && sudo apt-get -y dist-upgrade /
 && sudo apt-get -y install xserver-xorg xinit /
@@ -10,7 +11,7 @@ sudo apt-get update /
 && sudo apt-get -y autoremove /
 && sudo apt-get -y autoclean
 
-
+#Create PolicyKit File - This Alloww You to Shutdown and Restart the Pi from XFCE
 sudo su
 cat > /etc/polkit-1/localauthority/50-local.d/org.freedesktop.pkla << EOL
 [freedesktop]
@@ -34,6 +35,7 @@ Action=org.freedesktop.NetworkManager.*
 ResultAny=yes
 EOL
 
+#Create Ham Radio Menu Directory
 cat > /usr/share/desktop-directories/ham-radio.directory  << EOL
 [Desktop Entry]
 Version=1.0
@@ -45,28 +47,27 @@ EOL
 
 exit
 
+#Enables Tab Autocomplete Inside Remote Desktop
 sudo cp /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml.bak
-
 sudo sed -i -e 's/<property name="\&lt;Super\&gt;Tab" type="string" value="switch_window_key"\/>/<property name="\&lt;Super\&gt;Tab" type="empty"\/>/' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-
+#Enable wpa_gui to Make Changes to wpa_supplicant.conf
 sudo sed -i -e 's/Exec=\/usr\/sbin\/wpa_gui/Exec=sudo \/usr\/sbin\/wpa_gui/' /usr/share/applications/wpa_gui.desktop
-
+#Put wpa_gui on Desktop
 sudo cp /usr/share/applications/wpa_gui.desktop /home/pi/Desktop/wpa_gui.desktop
-
+#Enables Raspberry Pi Config App in XFCE
 sudo sed -i '/NotShowIn=/d' /usr/share/applications/rc_gui.desktop
-
+#Enables Synaptics Package Manager to Run
 sudo sed -i -e 's/Exec=synaptic-pkexec/Exec=sudo synaptic-pkexec/' /usr/share/applications/synaptic.desktop
-
+#Enables Raspberry Pi Clone App in XFCE
 sudo sed -i '/NotShowIn=/d' /usr/share/applications/piclone.desktop
-
+#Copies Custom Background to Correct Folder
 sudo cp /home/pi/Scripts/xfce-blue-utah.jpg /usr/share/backgrounds/xfce/xfce-blue-utah.jpg
-
+#Change lightdm Background to Custom Background
 sudo sed -i -e 's/background=\/usr\/share\/images\/desktop-base\/login-background.svg/background=\/usr\/share\/backgrounds\/xfce\/xfce-blue-utah.jpg/' /etc/lightdm/lightdm-gtk-greeter.conf
 
 # DV-Mega Mods
 sudo systemctl stop serial-getty@ttyAMA0.service
 sudo systemctl disable serial-getty@ttyAMA0.service
-
 sudo bash -c 'echo " " >> /boot/config.txt'
 sudo bash -c 'echo "# Disable Bluetooth on Raspberry Pi 3" >> /boot/config.txt'
 sudo bash -c 'echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt'
