078dd0477348986429dcedd7cf086be3526e8219
candrist <elderandrist@hotmail.com>
2016-06-07 18:46:39 -0600

    Update install_apps.sh
    added comments

--- install_apps.sh
+++ install_apps.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+#Directory Pre-Requisites
 mkdir -p /home/pi/Applications
 sudo mkdir -p /usr/local/var/log/opendv
 sudo chown pi:pi /usr/local/var/log/opendv
 sudo mkdir -p /usr/local/etc/opendv
 sudo chown pi:pi /usr/local/etc/opendv
-
+#Download and Install Arduino IDE
 wget -O /home/pi/Downloads/arduino-1.6.9-linuxarm.tar.xz http://www.dmr-utah.net/support/mmdvm/arduino-1.6.9-linuxarm.tar.xz
 tar xfv /home/pi/Downloads/arduino-1.6.9-linuxarm.tar.xz -C /home/pi/Applications/
 /home/pi/Applications/arduino-1.6.9/install.sh
@@ -14,13 +15,12 @@ sudo sed -i -e 's/Categories=Development;IDE;Electronics;/Categories=HamRadio/' 
 rm -Rf /home/pi/Desktop/arduino-arduinoide.desktop
 rm -Rf /home/pi/.gnome/apps/arduino-arduinoide.desktop
 rm -Rf /home/pi/Downloads/arduino-1.6.9-linuxarm.tar.xz
-
+#Download Source Code
 git clone https://github.com/g4klx/MMDVM.git /home/pi/Applications/MMDVM
 git clone https://github.com/g4klx/MMDVMHost.git /home/pi/Applications/MMDVMHost
 git clone https://github.com/g4klx/MMDVMCal.git /home/pi/Applications/MMDVMCal
 git clone https://github.com/dl5di/OpenDV.git /home/pi/Applications/OpenDV
-
-# Build ircDDBGateway
+#Build ircDDBGateway
 # cd /home/pi/Applications/OpenDV/ircDDBGateway
 # ./configure
 # make
