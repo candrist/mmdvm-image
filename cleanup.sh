e722c28b8bda38063c80bb9928b5f4680941c334
candrist <elderandrist@hotmail.com>
2016-10-24 19:48:46 -0600

    Update cleanup.sh
    DMRIds.dat cleanup added

--- cleanup.sh
+++ cleanup.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-#Stop MMDVMHost
+# Stop MMDVMHost
 sudo pkill -f /home/pi/Applications/MMDVMHost
-sudo pkill -f ircddbgateway
-sudo pkill -f ircddbgatewayconfig
-sudo pkill -f ircddbgw_conf
-#Remove Old MMDVM.ini
+# Remove Old MMDVM.ini
 rm -Rf /home/pi/MMDVM_*.ini
-#Remove Script Logs
+# Remove Script Logs
 rm -Rf /home/pi/Logs/*
-#Remove MMDVMHost Logs
+# Remove MMDVMHost Logs
 rm -Rf /home/pi/Applications/MMDVMHost/MMDVM-*.log
-#Make Sure Folders Exist and Have Correct Permissions for ircDDBGateway
-sudo chmod -R 0777 /home/opendv
-#sudo mkdir -p /usr/local/var/log/opendv
-#sudo chown pi:pi /usr/local/var/log/opendv
-#sudo mkdir -p /usr/local/etc/opendv
-#sudo chown pi:pi /usr/local/etc/opendv
-#Remove ircDDBGateway Config and Logs
-rm -Rf /home/pi/*.log
-#rm -Rf /usr/local/var/log/opendv/*
-#rm -Rf /usr/local/etc/opendv/*
-#Clear Bash History
+# Remove Old DMRIds.dat files
+rm -Rf /home/pi/Applications/MMDVMHost/DMRIds.dat.*
+# Clear Bash History
 cat /dev/null > /home/pi/.bash_history
 history -c
