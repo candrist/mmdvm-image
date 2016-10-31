3f87756c59d75e73e435fefd0c190be8dbc72baf
candrist <elderandrist@hotmail.com>
2016-10-30 19:19:36 -0600

    Update install_apps.sh
    Add Bash Shortcuts

--- install_apps.sh
+++ install_apps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#Directory Pre-Requisites
+# Directory Pre-Requisites
 mkdir -p /home/pi/Applications
-#Download and Install Arduino IDE
+# Download and Install Arduino IDE
 wget -O /home/pi/Downloads/arduino-1.6.12-linuxarm.tar.xz https://downloads.arduino.cc/arduino-1.6.12-linuxarm.tar.xz
 tar xfv /home/pi/Downloads/arduino-1.6.12-linuxarm.tar.xz -C /home/pi/Applications/
 /home/pi/Applications/arduino-1.6.12/install.sh
@@ -11,7 +11,11 @@ sudo sed -i -e 's:Categories=Development;IDE;Electronics;:Categories=AmateurRadi
 rm -Rf /home/pi/Desktop/arduino-arduinoide.desktop
 rm -Rf /home/pi/.gnome/apps/arduino-arduinoide.desktop
 rm -Rf /home/pi/Downloads/arduino-1.6.12-linuxarm.tar.xz
-#Download Source Code
+# Download Source Code
 git clone https://github.com/g4klx/MMDVM.git /home/pi/Applications/MMDVM
 git clone https://github.com/g4klx/MMDVMHost.git /home/pi/Applications/MMDVMHost
 git clone https://github.com/g4klx/MMDVMCal.git /home/pi/Applications/MMDVMCal
+# Add Bash Shortcut
+bash -c 'echo " " >> ~/.bash_profile'
+bash -c 'echo "# Shortcuts" >> ~/.bash_profile'
+bash -c 'echo "alias smm=\"/home/pi/Applications/MMDVMHost/MMDVMHost /home/pi/MMDVM.ini\"" >> ~/.bash_profile'
